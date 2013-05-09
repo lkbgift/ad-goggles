d55ce62ee796369767a5859abbc25f8fe8963fbe
lkbgift <lkbgift@gmail.com>
2013-05-09 11:44:35 -0400

    rails+foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adgoggles::Application.config.secret_key_base = '1a9ddfa84a19ed5e930e95883b58e8b9130850d03d58adf466b882360a54bd5960a9f52615f4df76859bc1ac19f08815eff7f610945d60e1d88efd3db32cdf74'
